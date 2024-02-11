7ac6d2757f5494b7eb49b0ae6e406afc7a4897b2
matte <matte@DESKTOP-NA3JRQD>
2024-02-11 10:11:43 +0100

    Aggiungere i file di progetto.

---
+++ somma vettori/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somma_vettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vettori v1 = new Vettori(3, 5);
+            Vettori v2 = new Vettori(4, 6);
+            Console.WriteLine(v1+v2);
+            Console.ReadLine();
+        }
+
+        
+
+
+
+    }
+}
---
+++ somma vettori/Vettori.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somma_vettori
+{
+    internal class Vettori
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vettori(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Vettori operator +( Vettori vec1, Vettori vec2)
+        {
+            return new Vettori(vec1.X + vec2.X, vec1.Y + vec2.Y);
+        }
+        public override string ToString() 
+        {
+            return string.Format("{0};{1}", X, Y);
+        }
+    }
+}
+
